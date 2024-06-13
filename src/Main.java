99abcb11e28f7ba6a08af8c21f8421c3260cb508
Bamz666 <muh.ibrahim00@gmail.com>
2024-06-13 13:53:34 +0700

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = "pemrogramanDasarJava1";
+
+        int jumlahHurufBesar = 0;
+        int jumlahHurufKecil = 0;
+        int jumlahAngka = 0;
+        int jumlahSimbol = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char karakter = input.charAt(i);
+
+            if (Character.isUpperCase(karakter)) {
+                jumlahHurufBesar++;
+            } else if (Character.isLowerCase(karakter)) {
+                jumlahHurufKecil++;
+            } else if (Character.isDigit(karakter)) {
+                jumlahAngka++;
+            } else {
+                jumlahSimbol++;
+            }
+        }
+
+        System.out.println("uppercase : " + jumlahHurufBesar);
+        System.out.println("lowecase : " + jumlahHurufKecil);
+        System.out.println("angka : " + jumlahAngka);
+        System.out.println("symbol : " + jumlahSimbol);
     }
 }
